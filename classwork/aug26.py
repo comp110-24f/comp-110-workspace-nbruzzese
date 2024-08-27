5885aed50995d0a4798548ba2fe79bcf94d3cf7c
nbruzzese <bruzzese@unc.edu>
2024-08-27 13:50:30 -0400

    aug26

---
+++ classwork/aug26.py
@@ -0,0 +1,15 @@
+"""Practice with functions"""
+
+from random import randint
+
+# print(randint(3, 17))
+
+
+# Function Definition
+def sum(num1: int, num2: int) -> int:
+    """return the sum of num1 and num2."""
+    return num1 + num2
+
+
+# Function Call
+print(sum(num1=11, num2=3))
