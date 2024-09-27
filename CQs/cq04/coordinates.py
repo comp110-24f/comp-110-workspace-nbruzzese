6c1285e5f77011f6ca156b4c71f940ea1c155181
nbruzzese <bruzzese@unc.edu>
2024-09-27 12:40:27 -0400

    finished cq04

--- CQs/cq04/coordinates.py
+++ CQs/cq04/coordinates.py
@@ -1,3 +1,17 @@
-"""EX03 - Wordle - The word guessing game"""
+"""A function for finding coordinates of words (imported to visualize.py)"""
 
 __author__ = "730649404"
+
+
+def get_coords(xs: str, ys: str) -> None:
+    """Prints formatted pairs of each character of input strings"""
+    xs_count: int = 0
+    # count for first str
+    while xs_count < len(xs):
+        ys_count: int = 0
+        # count for second str
+        while ys_count < len(ys):
+            print(f"({xs[xs_count]},{ys[ys_count]})")
+            # prints letters of second str for one letter of first str
+            ys_count = ys_count + 1
+        xs_count = xs_count + 1
